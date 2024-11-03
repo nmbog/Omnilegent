a521184c8f63809c941abf793438a3be3a646064
Nicole Marie <nicolemariebogart@gmail.com>
2024-11-02 18:04:48 -0600

    Added CREATE statement for Users table

---
+++ backend/database/DDL.sql
@@ -0,0 +1,21 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT=0;
+
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Authors;
+DROP TABLE IF EXISTS Genres;
+DROP TABLE IF EXISTS UserBookStatus;
+
+/* Create Users table */
+CREATE OR REPLACE TABLE Users (
+    userID int NOT NULL AUTO_INCREMENT UNIQUE, 
+    username varchar(45) NOT NULL UNIQUE,
+    email varchar(45) NOT NULL,
+    userPassword varchar(45) NOT NULL,
+    PRIMARY KEY (userID)
+);
+
+CREATE REPLACE TABLE Books (
+
+);
