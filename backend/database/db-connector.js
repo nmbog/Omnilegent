aab673e9ae131e3ae470cbb5a71af320f6946266
Nicole Marie <nicolemariebogart@gmail.com>
2024-11-06 11:23:30 -0700

    Checking DB connection

--- backend/database/db-connector.js
+++ backend/database/db-connector.js
@@ -6,5 +6,5 @@ var pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DATABASE
 });
